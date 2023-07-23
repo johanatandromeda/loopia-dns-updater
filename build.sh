6bd62949b8b76f5c3f2516b5415e50d57045bab7
Johan Hedin <johan.hedin@csamhealth.com>
2023-07-24 01:26:03 +0200

    Separate names for all builds

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ echo Buildning V$GIT_TAG
 
 FLAGS=""
 OUTPUT="loopia-dns-updater"
-env GOOS=linux GOARCH=amd64 go build -o linux/$OUTPUT $FLAGS -ldflags "-X main.version=$GIT_TAG" cmd/main/*.go
-env GOOS=openbsd GOARCH=amd64 go build -o openbsd/$OUTPUT $FLAGS -ldflags "-X main.version=$GIT_TAG" cmd/main/*.go
-env GOOS=freebsd GOARCH=amd64 go build -o freebsd/$OUTPUT $FLAGS -ldflags "-X main.version=$GIT_TAG" cmd/main/*.go
+env GOOS=linux GOARCH=amd64 go build -o linux/$OUTPUT-linux $FLAGS -ldflags "-X main.version=$GIT_TAG" cmd/main/*.go
+env GOOS=openbsd GOARCH=amd64 go build -o openbsd/$OUTPUT-openbsd $FLAGS -ldflags "-X main.version=$GIT_TAG" cmd/main/*.go
+env GOOS=freebsd GOARCH=amd64 go build -o freebsd/$OUTPUT-freebsd $FLAGS -ldflags "-X main.version=$GIT_TAG" cmd/main/*.go
